75136893d9e8fedb3f57c1f8e434752081fa6f18
Oluwaseyi Daniel <65072332+redmicelles@users.noreply.github.com>
2022-01-29 10:40:51 +0100

    Tailwind added

---
+++ frontend/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App bg-gradient-to-r from-sky-500 to-indigo-500">
+      <div class="grid gap-8 items-start justify-center">
+        <button class="px-7 py-4 bg-black rounded-lg leading-none flex items-center divide-x divide-gray-600">
+        <span class="pr-6 text-gray-100">CloudJamz</span>
+        <span class="pl-6 text-indigo-400">Get in here &rarr;</span>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
